75617c7f030c09a68da6adbf1b0c664c91936cec
xiaoo <mapeleaf@126.com>
2018-05-23 09:25:25 +0200

    finish coodinate checking but fail with input suitable value in W and P

--- lab1/DFS.m
+++ lab1/DFS.m
@@ -1,34 +1,52 @@
 %stack
+%depth first
 clear ; close all;
 start = [1, 1];
 goal = [10, 10];
 Map = Map_generate(start, goal)
 
-syms s t r pred W P test
-W = zeros();
-P = zeros();
-pred = zeros();
+syms s t r pred W P
+
+P = []; %已遍历
+pred = []; %结果
 % r is false
 r = 0;
-% s = start;
-s = [0,0];
 t = goal;
-W = s;
+W = []; %待遍历
+W = start;
 
 while(~isempty(W) && (r == 0))
-    test =[];
-    for i = 1:4
-        a = mod(i,2) * (-1) + (mod(i,2) - 1) * (-1);%奇数取1，偶数取-1
-        %>2取0，<2取1
-        if(i > 2)
-            b = 0;
-        else
-            b = 1;
-        end
-
-%         A = [s(1) - a * b, s(2) + a * b]  %待修改
-        test = [test; A];
-%         test(s(1) + a * b, s(2) + a * b) = 1;
+%     栈，后进先出
+    s = W(size(W,1),:);
+%     判断是找到目标
+    if s == t
+        r = 1;
+    else
+        P = [P;s];
     end
-
+    
+%     for: 遍历附近的坐标，并选取在地图范围内的坐标
+    for i = 1:4
+        a = mod(i,2) * (-1) + (mod(i,2) - 1) * (-1);%奇数取-1，偶数取1
+        %>2取(0,1),<2取(1,0)
+        if(i > 2)
+            y = 1; x = 0;
+        else
+            x = 1; y = 0;
+        end
+        %操作a,x,y使得A = [-1,0;1,0;-1,-1;-1,1]
+        A = [s(1) + a * x, s(2) + a * y];
+        if A(1) > 0 && A(1) <= length(Map) && A(2) > 0 && A(2) <= length(Map)
+%             未成功+++++++
+            for i = 1:length(W)
+               for j = 1:length(P)
+%                    向量的与或运算用&和|
+                  if A ~= W(i) & A ~= P(j)
+                      pred = [pred; A];
+                      W = [W;A]; 
+                  end
+               end
+            end
+        end                   
+    end
 end
--- lab1/Map_generate.m
+++ lab1/Map_generate.m
@@ -1,10 +1,10 @@
 function Map = Map_generate(start,goal)
-Start = [1, 1];
-goal = [10, 10];
+
 % clear; close all;
 % rng - 矛
 % rng(100);
-Map = round( rand(10,10) > 0.3);
+Map = round( rand(goal) > 0.3);
+% 使得起始点和目标点不为空
 if Map(start) == 0
     Map(start) = 1;
 end
